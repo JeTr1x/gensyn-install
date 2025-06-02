91f3b093f22841e497130f3af921d0b2d204d9e0
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2025-06-02 22:16:32 +0300

    Create prep.sh

---
+++ prep.sh
@@ -0,0 +1,17 @@
+
+
+
+# Подготовка сервера
+
+sudo apt install -y python3 python3-venv python3-pip curl screen git yarn
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install -y yarn
+
+sudo apt-get update
+curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash -
+sudo apt-get install -y nodejs
+node -v
+sudo npm install -g yarn
+yarn -v
