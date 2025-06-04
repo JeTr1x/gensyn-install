71b0963561c687736e6ea8e7fd48fa46b04a35f9
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2025-06-04 14:20:51 +0300

    Update gensyn_install.sh

--- gensyn_install.sh
+++ gensyn_install.sh
@@ -1,4 +1,6 @@
 
+CONFIG_NUM=$1
+
 # Подготовка сервера
 echo "Подготовка сервера"
 bash <(wget -qO- https://raw.githubusercontent.com/JeTr1x/gensyn-install/refs/heads/main/prep.sh)
@@ -12,6 +14,9 @@ cd rl-swarm && python3 -m venv .venv
 # Скачивание кастомного скрипта на запуск ноды
 echo " Скачивание кастомного скрипта на запуск ноды"
 curl -o /root/rl-swarm/run_rl_swarm_true.sh https://raw.githubusercontent.com/JeTr1x/gensyn-install/refs/heads/main/run_rl_swarm_true.sh
+
+# Скачивание кастомного конфига для ноды
+curl -o /root/rl-swarm/hivemind_exp/configs/mac/grpo-qwen-2.5-0.5b-deepseek-r1.yaml https://raw.githubusercontent.com/JeTr1x/gensyn-install/refs/heads/main/cpu_config_${CONFIG_NUM}.yaml
 
 # Скачивание сервисника
 curl -o /etc/systemd/system/rl-swarm.service https://example.com/rl-swarm.service
