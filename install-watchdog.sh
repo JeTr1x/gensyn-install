fe0b12d1539f56609e193938e5d17fc128b38385
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2025-07-10 15:38:38 +0300

    Update install-watchdog.sh

--- install-watchdog.sh
+++ install-watchdog.sh
@@ -1,6 +1,6 @@
 cd $HOME/rl-swarm && \
-wget https://raw.githubusercontent.com/JeTr1x/gensyn-install/refs/heads/main/rl-swarm-watchdog.sh && \
-chmod +x rl-swarm-watchdog.sh && \
+curl -o /root/rl-swarm/rl-swarm-watchdog.sh https://raw.githubusercontent.com/JeTr1x/gensyn-install/refs/heads/main/rl-swarm-watchdog.sh && \
+chmod +x /root/rl-swarm/rl-swarm-watchdog.sh && \
 curl -o /etc/systemd/system/rl-swarm-watchdog.service https://raw.githubusercontent.com/JeTr1x/gensyn-install/refs/heads/main/rl-swarm-watchdog.service && \
 systemctl daemon-reload && \
 systemctl enable rl-swarm-watchdog.service && \
