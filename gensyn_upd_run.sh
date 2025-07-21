d3f4e4658b8c79612a61f4350a703be0a3bce442
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2025-07-21 18:05:39 +0300

    Update gensyn_upd_run.sh

--- gensyn_upd_run.sh
+++ gensyn_upd_run.sh
@@ -3,6 +3,7 @@
 
 # Скачивание кастомного скрипта на запуск ноды
 echo " Скачивание кастомного скрипта на запуск ноды"
+systemctl stop rl-swarm
 curl -o /root/rl-swarm/run_rl_swarm_true.sh https://raw.githubusercontent.com/JeTr1x/gensyn-install/refs/heads/main/run_rl_swarm_true.sh
 
 rm /root/rl-swarm/userApiKey.json /root/rl-swarm/userData.json && systemctl restart rl-swarm
