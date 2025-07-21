552fbae98596fecfbf16ad322c05fc724bc91fbf
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2025-07-21 18:05:03 +0300

    Create gensyn_change_run.sh

---
+++ gensyn_change_run.sh
@@ -0,0 +1,10 @@
+
+
+
+# Скачивание кастомного скрипта на запуск ноды
+echo " Скачивание кастомного скрипта на запуск ноды"
+curl -o /root/rl-swarm/run_rl_swarm_true.sh https://raw.githubusercontent.com/JeTr1x/gensyn-install/refs/heads/main/run_rl_swarm_true.sh
+
+rm /root/rl-swarm/userApiKey.json /root/rl-swarm/userData.json && systemctl restart rl-swarm
+journalctl -fu rl-swarm
+
